d03b75cdd323aec773606ef1bbe870720c1c3562
Ross Girshick <rbg@cs.uchicago.edu>
2012-02-18 13:43:44 -0600

    some cleanup

--- fv_cache/mempool.h
+++ fv_cache/mempool.h
@@ -2,35 +2,52 @@
 #define MEMPOOL_H
 
 #include <iostream>
-#include <queue>
+#include <stack>
 #include <tr1/unordered_map>
 
 using namespace std;
 using namespace std::tr1;
 
+/** -----------------------------------------------------------------
+ ** A very simple memory pool. Arrays are pooled by exact dimension,
+ ** which is fine for the feature vector cache because there are 
+ ** typically only a small number -- e.g., 3 -- of distinct array 
+ ** sizes. This is not a good strategy in general, but here the goal
+ ** is simplicity.
+ */
+
 template<class T>
 struct mempool {
-  typedef unordered_map<int, queue<T *> > type;
-  typedef typename type::iterator iter;
+  typedef unordered_map<int, stack<T *> > pool_type;
+  typedef typename pool_type::iterator pool_iter;
 
-  typename mempool<T>::type pool;
+  pool_type pool;
   int count;
   int reuse;
 
+
+  /** ---------------------------------------------------------------
+   ** Constructor
+   */
   mempool() {
     count = 0;
     reuse = 0;
   }
 
+
+  /** ---------------------------------------------------------------
+   ** Get a dim-length array of T's either by allocating new memory
+   ** or pulling an existing free array from the pool
+   */
   T *get(int dim) {
-    typename mempool<T>::iter i = pool.find(dim);
+    pool_iter i = pool.find(dim);
     T *data = NULL;
 
     if (i == pool.end() || i->second.empty()) {
       data = new (nothrow) T[dim];
       count++;
     } else {
-      data = i->second.front();
+      data = i->second.top();
       i->second.pop();
       reuse++;
     }
@@ -38,11 +55,16 @@ struct mempool {
     return data;
   }
 
+
+  /** ---------------------------------------------------------------
+   ** Put the dim length array data into the memory pool for future
+   ** reuse
+   */
   void put(int dim, T *data) {
-    typename mempool<T>::iter i = pool.find(dim);
+    pool_iter i = pool.find(dim);
 
     if (i == pool.end()) {
-      queue<T *> q;
+      stack<T *> q;
       q.push(data);
       pool[dim] = q;
     } else {
@@ -50,27 +72,32 @@ struct mempool {
     }
   }
 
-  void print() {
-    cout << "Pool has allocated " << count << " elements in " 
-         << pool.size() << " buckets (reuse: " << reuse << ")" << endl;
-    typename mempool<T>::iter i, i_end;
-    for (i = pool.begin(), i_end = pool.end(); i != i_end; ++i) {
-      cout << " bucket: " << i->first << " has " << i->second.size() 
-           << " elements" << endl;
-    }
-  }
 
+  /** ---------------------------------------------------------------
+   ** Free all memory allocated by the pool
+   */
   void free() {
     count = 0;
     reuse = 0;
 
-    typename mempool<T>::iter i, i_end;
+    pool_iter i, i_end;
     for (i = pool.begin(), i_end = pool.end(); i != i_end; ++i) {
       while (!i->second.empty()) {
-        T *data = i->second.front();
+        T *data = i->second.top();
         i->second.pop();
         delete [] data;
       }
+    }
+  }
+
+
+  void print() {
+    cout << "Pool has allocated " << count << " elements in " 
+         << pool.size() << " buckets (reuse: " << reuse << ")" << endl;
+    pool_iter i, i_end;
+    for (i = pool.begin(), i_end = pool.end(); i != i_end; ++i) {
+      cout << " bucket: " << i->first << " has " << i->second.size() 
+           << " elements" << endl;
     }
   }
 };
