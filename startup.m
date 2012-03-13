db5102185a399e00bbac83182998c237c0244173
Ross Girshick <ross.girshick@gmail.com>
2012-03-13 16:39:03 -0500

    Print AP scores, but don't return them (pascal.m); Avoid adding .git to matlab's path to speed up startup (startup.m)

--- startup.m
+++ startup.m
@@ -1,3 +1,9 @@
-addpath(genpath('.'));
+% Avoiding addpath(genpath('.')) because .git gets included
+% a large number of subdirectories, which makes startup slow
+incl = {'rescore', 'bbox_pred', 'fv_cache', ...
+        'bin', 'gdetect', 'utils'};
+for i = 1:length(incl)
+  addpath(genpath(incl{i}));
+end
 conf = voc_config();
 fprintf('%s is set up\n', conf.version);
