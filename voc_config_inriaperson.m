470da14a9984c32499a7587ffa518bff669ed60d
Ross Girshick <ross.girshick@gmail.com>
2012-05-21 14:48:50 -0500

    2010 cascade data

--- voc_config_inriaperson.m
+++ voc_config_inriaperson.m
@@ -1,8 +1,10 @@
 function conf = voc_config_inriaperson()
-% Sample config override file
+% Configuration used for training the INRIA person model
 %
 % To use this execute:
 %  >> global VOC_CONFIG_OVERRIDE;
-%  >> VOC_CONFIG_OVERRIDE = @sample_voc_config_override;
+%  >> VOC_CONFIG_OVERRIDE = @voc_config_inriaperson;
+
+% SEE: INRIA/README for more details
 
 conf.training.C = 0.006;
