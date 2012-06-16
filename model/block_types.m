3bbe256c99cf47b878290dac9eae5b7e5b32f021
Ross Girshick <ross.girshick@gmail.com>
2012-06-16 16:05:42 -0700

    don't use enumerations for compatibility with older matlabs; convert pretrained models

--- model/block_types.m
+++ model/block_types.m
@@ -1,26 +1,24 @@
-% Note: Enumerations are only available in matlab >= 2010b
 % Symbols for the various types of parameter blocks in a model
-classdef block_types
-  enumeration
-    Filter,       % HOG filter
-    PCAFilter,    % HOG PCA filter (used by cascade)
-    SepQuadDef,   % Separable Quadratic Deformation
-    Other         % Other: offets, ...
+classdef (Sealed) block_types
+  properties  (Constant)
+    Other       = 'O';
+    Filter      = 'F';
+    PCAFilter   = 'P';
+    SepQuadDef  = 'D';
+  end
+  methods (Access = private)
+    function out = block_types
+    end
   end
 end
 
-% MATLAB < 2010b users: You can use this code instead, though
-% you will not be able to load the pre-trained models.
+% Note: Enumerations are only available in matlab >= 2010b
 %
-%classdef (Sealed) block_types
-%  properties  (Constant)
-%    Filter      = 1;
-%    PCAFilter   = 2;
-%    SepQuadDef  = 3;
-%    Other       = 4;
-%  end
-%  methods (Access = private)
-%    function out = block_types
-%    end
+%classdef block_types
+%  enumeration
+%    Filter,       % HOG filter
+%    PCAFilter,    % HOG PCA filter (used by cascade)
+%    SepQuadDef,   % Separable Quadratic Deformation
+%    Other         % Other: offets, ...
 %  end
 %end
--- model/model_types.m
+++ model/model_types.m
@@ -1,23 +1,21 @@
-% Note: Enumerations are only available in matlab >= 2010b
 % Model types:
 %   MixStar   Mixture of star models with latent orientation
 %   Grammar   General grammar model (no specific structure)
-classdef model_types
-  enumeration
-    MixStar, Grammar
+classdef (Sealed) model_types
+  properties  (Constant)
+    MixStar = 'M';
+    Grammar = 'G';
+  end
+  methods (Access = private)
+    function out = model_types
+    end
   end
 end
 
-% MATLAB < 2010b users: You can use this code instead, though
-% you will not be able to load the pre-trained models.
+% Note: Enumerations are only available in matlab >= 2010b
 %
-%classdef (Sealed) model_types
-%  properties  (Constant)
-%    MixStar = 1;
-%    Grammar = 2;
-%  end
-%  methods (Access = private)
-%    function out = model_types
-%    end
+%classdef model_types
+%  enumeration
+%    MixStar, Grammar
 %  end
 %end
