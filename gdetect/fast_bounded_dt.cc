ee341bc747e5410cf9732dc7f36d8a342403df9d
Ross Girshick <rbg@eecs.berkeley.edu>
2013-07-19 10:44:02 -0700

    1.2x faster fast_bounded_dt.cc

--- gdetect/fast_bounded_dt.cc
+++ gdetect/fast_bounded_dt.cc
@@ -19,6 +19,8 @@
 #include <sys/types.h>
 #include <algorithm>
 
+using namespace std;
+
 // Generalized distance transforms based on Felzenszwalb and Huttenlocher's
 // "Distance Transform of Sampled Functions." Theory of Computing, Vol. 8,
 // No. 19, 2012.
@@ -26,38 +28,47 @@
 // The cost function implemented here is a bounded quadratic:
 //   d(x) = a*x^2 + b*x   if x \in [-range, range]
 //   d(x) = inf           otherwise
+//
+// Some ideas for speeding up this code (i.e. precomputing division factors) 
+// were taken from Charles Dubout's ffld code (http://www.idiap.ch/~cdubout/
+// code/ffld.tar.gz).
 
 static double eps = 0.00001;
 
 static inline int square(int x) { return x*x; }
 
 void dt1d(const double *src, double *dst, int *ptr, 
-          int step, int n, double a, double b, double range) {
-  int    *v = new int[n];
-  double *z = new double[n+1];
+          int step, int n, double a, double b, double range,
+          int *v, double *z, double *t) {
   int k     = 0;
   v[0]      = 0;
   z[0]      = -INFINITY;
   z[1]      = +INFINITY;
+  
+  double a_inv = 1/a;
+
   for (int q = 1; q <= n-1; q++) {
-    double q2 = q*q; 
     // compute unbounded point of intersection
-    double s = ((src[q*step] - src[v[k]*step]) 
-                 - b*(q - v[k]) + a*(q2 - square(v[k]))) / (2*a*(q-v[k]));
+    double s = 0.5 * ((src[q*step] - src[v[k]*step]) * t[q - v[k]] 
+                      + q + v[k] 
+                      - b * a_inv);
+
     // bound point of intersection; +/- eps to handle boundary conditions
-    s = std::min(v[k]+range+eps, std::max(q-range-eps, s));
+    s = min(v[k]+range+eps, max(q-range-eps, s));
+
     while (s <= z[k]) {
       // delete dominiated parabola
       k--;
-      s = ((src[q*step] - src[v[k]*step]) 
-           - b*(q - v[k]) + a*(q2 - square(v[k]))) / (2*a*(q-v[k]));
-      s = std::min(v[k]+range+eps, std::max(q-range-eps, s));
+      s = 0.5 * ((src[q*step] - src[v[k]*step]) * t[q - v[k]] 
+                  + q + v[k] 
+                  - b * a_inv);
+      s = min(v[k]+range+eps, max(q-range-eps, s));
     }
     k++;
     v[k]   = q;
     z[k]   = s;
-    z[k+1] = INFINITY;
   }
+  z[k+1] = INFINITY;
 
   k = 0;
   for (int q = 0; q <= n-1; q++) {
@@ -66,13 +77,10 @@ void dt1d(const double *src, double *dst, int *ptr,
     dst[q*step] = a*square(q-v[k]) + b*(q-v[k]) + src[v[k]*step];
     ptr[q*step] = v[k];
   }
-
-  delete [] v;
-  delete [] z;
 }
 
 // matlab entry point
-// [M, Ix, Iy] = fast_dt(vals, ax, bx, ay, by, range)
+// [M, Ix, Iy] = fast_bounded_dt(vals, ax, bx, ay, by, range)
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) { 
   if (nrhs != 6)
     mexErrMsgTxt("Wrong number of inputs"); 
@@ -109,13 +117,29 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   int32_t *tmpIx = (int32_t *)mxCalloc(dims[0]*dims[1], sizeof(int32_t));
   int32_t *tmpIy = (int32_t *)mxCalloc(dims[0]*dims[1], sizeof(int32_t));
 
-  for (int x = 0; x < dims[1]; x++)
-    dt1d(vals+x*dims[0], tmpM+x*dims[0], tmpIy+x*dims[0], 1, dims[0], -ay, -by, range);
+  // temporary storage used by 1d distance transforms
+  int *v = new int[max(dims[0], dims[1])];
+  double *z = new double[max(dims[0], dims[1]) + 1];
+  double *t = new double[max(dims[0], dims[1])];
   
-  for (int y = 0; y < dims[0]; y++)
-    dt1d(tmpM+y, M+y, tmpIx+y, dims[0], dims[1], -ax, -bx, range);
+  // cache divisive factors used in 1d distance transforms
+  t[0] = INFINITY;
+  for (int y = 1; y < dims[0]; y++)
+    t[y] = 1 / (-ay * y);
 
-  // get argmins and adjust for matlab indexing from 1
+  for (int x = 0; x < dims[1]; x++)
+    dt1d(vals+x*dims[0], tmpM+x*dims[0], tmpIy+x*dims[0], 1, dims[0], 
+         -ay, -by, range, v, z, t);
+  
+  // cache divisive factors used in 1d distance transforms
+  for (int x = 1; x < dims[1]; x++)
+    t[x] = 1 / (-ax * x);
+
+  for (int y = 0; y < dims[0]; y++)
+    dt1d(tmpM+y, M+y, tmpIx+y, dims[0], dims[1], 
+         -ax, -bx, range, v, z, t);
+
+  // get argmaxes and adjust for matlab indexing from 1
   for (int x = 0; x < dims[1]; x++) {
     for (int y = 0; y < dims[0]; y++) {
       int p = x*dims[0]+y;
@@ -123,6 +147,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
       Iy[p] = tmpIy[tmpIx[p]*dims[0]+y]+1;
     }
   }
+
+  delete [] v;
+  delete [] z;
 
   mxFree(tmpM);
   mxFree(tmpIx);
